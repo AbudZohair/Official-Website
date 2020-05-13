ba2711de8987ea68d7ca6865b1b497955471d876
Abud Zohair <abud.zohair@gmail.com>
2020-05-13 22:16:46 +0200

    Add bootstrap Bundle JS
    - add bootstrap js to make the bootstrap carousel interactive

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "styles.css";
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
